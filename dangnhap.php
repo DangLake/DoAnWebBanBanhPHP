f3d2c934c54faf4854f0ecb2d983c3c76bd658ec
DangLake <vthgiang181003@gmail.com>
2024-12-07 11:16:21 +0700

    sua lai dang nhap

--- dangnhap.php
+++ dangnhap.php
@@ -38,10 +38,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 // Kiểm tra vai trò và điều hướng
                 if ($user['role'] == 0) {
                     // Admin
-                    session_unset(); 
-                    session_destroy(); 
+                    session_unset(); // Hủy tất cả session
+                    session_destroy(); // Hủy session
 
-                 } else {
+                    echo '<script>alert("Đăng nhập thành công với vai trò Admin!"); window.location.href = "./admin/admin.php";</script>';
+                } else {
                     // User
                     echo '<script>alert("Đăng nhập thành công!"); window.location.href = "index.php";</script>';
                 }
